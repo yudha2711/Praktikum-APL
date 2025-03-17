a08887fc57b341d0595a637ebe848903803adc5d
unknown <abhistamaul@gmail.com>
2025-03-17 22:58:25 +0800

    Finish Posttest 2

---
+++ Posttest/Posttest-2-apl/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+while (true) {
+                        cout << "\n=== MENU ADMIN ===\n";
+                        cout << "1. Lihat Aktivitas Login\n2. Hapus User\n3. Exit ke Menu Utama\nPilih: ";
+                        cin >> choice;
+                        cin.ignore();
